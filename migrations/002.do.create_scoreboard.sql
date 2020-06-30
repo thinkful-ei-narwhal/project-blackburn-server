5e56f06ee6b93264ebc0476174c12a7c2eba5c7a
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-06-30 08:30:04 -0700

    migrations-jacob

---
+++ migrations/002.do.create_scoreboard.sql
@@ -0,0 +1,13 @@
+CREATE TYPE difficulty AS ENUM ('easy', 'medium', 'hard')
+
+CREATE TABLE scoreboard(
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER REFERENCES "user"(id)
+    ON DELETE CASCADE NOT NULL,
+  "story_id" TEXT NOT NULL,
+  "difficulty_id" difficulty,
+  "score" INTEGER NOT NULL,
+  "wpm" INTEGER NOT NULL,
+  "accuracy" INTEGER NOT NULL
+);
+
