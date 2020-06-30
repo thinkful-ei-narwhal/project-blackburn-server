a1e03ef1eb3c4844acb404ce633d2d15ebb16b39
Tyler Sharp <johntylersharp@gmail.com>
2020-06-30 11:14:27 -0400

    Added knex and db connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 require("dotenv").config();
-const app = require("./app");
-const { PORT, NODE_ENV } = require("./config");
 
-app.listen(PORT, () => console.log(`Server running in ${NODE_ENV} on ${PORT}`));
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
+
+const db = knex({
+  client: "pg",
+  connection: DATABASE_URL,
+});
+
+app.set("db", db);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
