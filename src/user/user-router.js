ad64ba135781d08e2ab80e38d6b75694ce410b99
Ren F <renfinn89@gmail.com>
2020-07-01 13:39:33 -0700

    patch route

--- src/user/user-router.js
+++ src/user/user-router.js
@@ -1,49 +1,68 @@
-const express = require("express");
-const path = require("path");
-const UserService = require("./user-service");
+const express = require('express');
+const path = require('path');
+const UserService = require('./user-service');
+const { requireAuth } = require('../middleware/jwt-auth');
 
 const userRouter = express.Router();
 const jsonBodyParser = express.json();
 
-userRouter.post("/", jsonBodyParser, async (req, res, next) => {
-  const { password, username, avatar } = req.body;
+userRouter
+  .post('/', jsonBodyParser, async (req, res, next) => {
+    const { password, username, avatar } = req.body;
 
-  for (const field of ["username", "password"])
-    if (!req.body[field])
-      return res.status(400).json({
-        error: `Missing '${field}' in request body`,
-      });
+    for (const field of ['username', 'password'])
+      if (!req.body[field])
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`,
+        });
 
-  try {
-    const passwordError = UserService.validatePassword(password);
+    try {
+      const passwordError = UserService.validatePassword(password);
 
-    if (passwordError) return res.status(400).json({ error: passwordError });
+      if (passwordError) return res.status(400).json({ error: passwordError });
 
-    const hasUserWithUserName = await UserService.hasUserWithUserName(
-      req.app.get("db"),
-      username
-    );
+      const hasUserWithUserName = await UserService.hasUserWithUserName(
+        req.app.get('db'),
+        username
+      );
 
-    if (hasUserWithUserName)
-      return res.status(400).json({ error: `Username already taken` });
+      if (hasUserWithUserName)
+        return res.status(400).json({ error: `Username already taken` });
 
-    const hashedPassword = await UserService.hashPassword(password);
+      const hashedPassword = await UserService.hashPassword(password);
 
-    const newUser = {
-      username,
-      password: hashedPassword,
-      avatar,
-    };
+      const newUser = {
+        username,
+        password: hashedPassword,
+        avatar,
+      };
 
-    const user = await UserService.insertUser(req.app.get("db"), newUser);
+      const user = await UserService.insertUser(req.app.get('db'), newUser);
 
-    res
-      .status(201)
-      .location(path.posix.join(req.originalUrl, `/${user.id}`))
-      .json(UserService.serializeUser(user));
-  } catch (error) {
-    next(error);
-  }
-});
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${user.id}`))
+        .json(UserService.serializeUser(user));
+    } catch (error) {
+      next(error);
+    }
+  })
+  .patch('/edit', requireAuth, jsonBodyParser, (req, res, next) => {
+    const { username, avatar } = req.body;
+    const updateUser = { username, avatar };
+
+    for (const [key, value] of Object.entries(updateUser))
+      if (value === null)
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`,
+        });
+    updateUser.id = req.user.id;
+    console.log('hello', updateUser.id, req.user.id);
+    UserService.updateUser(req.app.get('db'), req.user.id, updateUser)
+      .then((user) => {
+        res.status(201).json(user);
+      })
+      .catch(next);
+  });
 
 module.exports = userRouter;
--- src/user/user-service.js
+++ src/user/user-service.js
@@ -1,10 +1,10 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 
 const UserService = {
   hasUserWithUserName(db, username) {
-    return db("users")
+    return db('users')
       .where({ username })
       .first()
       .then((user) => !!user);
@@ -12,22 +12,28 @@ const UserService = {
   insertUser(db, newUser) {
     return db
       .insert(newUser)
-      .into("users")
-      .returning("*")
+      .into('users')
+      .returning('*')
       .then(([user]) => user);
+  },
+  updateUser(db, id, updateUser) {
+    return db('users')
+      .where('users.id', id)
+      .update(updateUser)
+      .returning(['username', 'avatar']);
   },
   validatePassword(password) {
     if (password.length < 8) {
-      return "Password be longer than 8 characters";
+      return 'Password be longer than 8 characters';
     }
     if (password.length > 72) {
-      return "Password be less than 72 characters";
+      return 'Password be less than 72 characters';
     }
-    if (password.startsWith(" ") || password.endsWith(" ")) {
-      return "Password must not start or end with empty spaces";
+    if (password.startsWith(' ') || password.endsWith(' ')) {
+      return 'Password must not start or end with empty spaces';
     }
     if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-      return "Password must contain one upper case, lower case, number and special character";
+      return 'Password must contain one upper case, lower case, number and special character';
     }
     return null;
   },
