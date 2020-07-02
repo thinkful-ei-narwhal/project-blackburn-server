594715ef6156a84e6a113b27485f69301c5d33f7
Tyler Sharp <johntylersharp@gmail.com>
2020-07-02 14:07:57 -0400

    wrong export on story, changed

--- src/story/story-router.js
+++ src/story/story-router.js
@@ -31,13 +31,6 @@ storyRouter.get("/:story_id", async (req, res, next) => {
   }
 });
 module.exports = storyRouter;
-
-<<<<<<< HEAD
-export default storyRouter;
-
-=======
-module.exports = storyRouter
->>>>>>> deploy
 //start page grabs ALL of story table
 //each INDIVIDUAL story is a GET request for story
 //join tables on every reference key
